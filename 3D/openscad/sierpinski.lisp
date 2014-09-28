b0e50f4baf65578119d1bd8add0ed7a6475a2521
David Rysdam <david@rysdam.org>
2014-09-27 22:10:55 -0400

    got the second layer working, but not with the original. also, need a driver.

--- 3D/openscad/sierpinski.lisp
+++ 3D/openscad/sierpinski.lisp
@@ -6,14 +6,14 @@
   (let ((newz (+ z (* side-length (sqrt (/ 2 3)))))
 		(rad (* side-length (/ (sqrt 3) 3))))
 	(list (list x y z)
-		  (list (* rad (sin (/ (* 0 2 pi) 3)))
-				(* rad (cos (/ (* 0 2 pi) 3)))
+		  (list (* rad (cos (/ (* 0 2 pi) 3)))
+				(* rad (sin (/ (* 0 2 pi) 3)))
 				newz)
-		  (list (* rad (sin (/ (* 1 2 pi) 3)))
-				(* rad (cos (/ (* 1 2 pi) 3)))
+		  (list (* rad (cos (/ (* 1 2 pi) 3)))
+				(* rad (sin (/ (* 1 2 pi) 3)))
 				newz)
-		  (list (* rad (sin (/ (* 2 2 pi) 3)))
-				(* rad (cos (/ (* 2 2 pi) 3)))
+		  (list (* rad (cos (/ (* 2 2 pi) 3)))
+				(* rad (sin (/ (* 2 2 pi) 3)))
 				newz))))
 
 (defun draw-tetrahedron (tetra)
@@ -26,7 +26,35 @@
 	 (scad:line-xyz p3 p4 *thickness*)
 	 (scad:line-xyz p4 p2 *thickness*))))
 
-(emit 
+(defun outermost-three (tetra)
+  (let ((largest-x (loop for p in tetra
+					  maximize (first p)))
+		(largest-y (loop for p in tetra
+					  maximize (second p)))
+		(smallest-y (loop for p in tetra
+					   minimize (second p))))
+	(loop for p in tetra
+	   when (or 
+			 (= (first p) largest-x)
+			 (= (second p) largest-y)
+			 (= (second p) smallest-y))
+	   append (list p))))
+
+(defun place-tetrahedron (tetra points) 
+  (loop for p in points
+	 collecting (scad:translate (first p)
+								(second p)
+								(third p) 
+								tetra)))
+
+(scad:emit 
  (draw-tetrahedron (make-tetrahedron 0 0 0 100)) 
+ :includes '("coordinates.scad")
+ :file "sierpinski.scad")
+
+(scad:emit 
+ (place-tetrahedron (draw-tetrahedron (make-tetrahedron 0 0 0 100))
+					(outermost-three 
+					 (make-tetrahedron 0 0 0 100)))
  :includes '("coordinates.scad")
  :file "sierpinski.scad")
