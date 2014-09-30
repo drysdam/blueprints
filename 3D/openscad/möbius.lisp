d6687fc5b9bdf67db08882a0e3552a2a2286b00c
David Rysdam <david@rysdam.org>
2014-09-29 21:33:47 -0400

    perfect!

--- 3D/openscad/möbius.lisp
+++ 3D/openscad/möbius.lisp
@@ -1,5 +1,20 @@
 (load "opensexp.lisp")
 
+(defun pairwise-hull (lst)
+  (let ((lst1 lst)
+		(lst2 (append (cdr lst) (list (car lst)))))
+	(mapcar #'scad:hull lst1 lst2)))
+
+(defun bar (length thickness)
+  (scad:translate (/ thickness -2) 0 0 
+			 (scad:hull
+			  (scad:translate 0 0 (/ length 2) 
+							  (scad:rotate 0 90 0 
+										   (scad:cylinder 1 thickness)))
+			  (scad:translate 0 0 (/ length -2) 
+							  (scad:rotate 0 90 0 
+										   (scad:cylinder 1 thickness))))))
+
 (defun loop-de-loop (height radius elements flip)
   (let ((width (/ (* 2 pi radius) elements))
 		(elem-flip-angle (/ flip elements))
@@ -9,7 +24,10 @@
 		 (scad:rotate 0 0 (* i elem-face-angle)
 			  (scad:translate 0 radius 0 
 				  (scad:rotate (* i elem-flip-angle) 0 0
-					   (scad:cube width 2 height :center 't)))))))
+							   (bar height width)))))))
 
-(emit (loop-de-loop 10 30 100 180)
+(emit (bar 10 2)
+	  :file "möbius.scad")
+
+(emit (pairwise-hull (loop-de-loop 10 30 50 180))
 	  :file "möbius.scad")
