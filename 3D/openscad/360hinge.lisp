9f55bfebab7854face394f0c584047c12a7c6bac
David Rysdam <david@rysdam.org>
2014-10-13 19:51:09 -0400

    putting one kinda semi-hinge on

--- 3D/openscad/360hinge.lisp
+++ 3D/openscad/360hinge.lisp
@@ -1,24 +1,49 @@
 (load "~/software/blueprints/3D/openscad/opensexp.lisp")
 
-(defun equilateral-triangle (side-length)
+(defparameter *side-length* 10)
+(defparameter *thickness* 2)
+
+(defun hinged-triangle ()
+  (scad:scad-union 
+   (triangle)
+   (scad:translate (* *side-length* (/ (sqrt 3) -6)) 0 0 
+   				   (scad:cube (/ *side-length* 4) 
+   							  (/ *side-length* 2)
+   							  (/ *thickness* 4) :center t))
+   (scad:translate (* *side-length* (/ (sqrt 3) -4)) 0 0 
+				   (scad:cube (/ *side-length* 10) 
+							  (/ *side-length* 2)
+							  (/ *thickness* 2) :center t))))
+
+(defun notched-triangle ()
   ; since I'm making my triangle by "radiating from the center" one
   ; unit, I need to scale by something to get the right side length
   (scad:difference
-   (let ((s (* side-length (/ (sqrt 3) 3))))
-	 (scad:scale s s 1 
+   (let ((s (* *side-length* (/ (sqrt 3) 3))))
+	 (scad:scale s s *thickness*
 				 (scad:polygon 
 				  (loop for i from 0 to 2 
 					 collecting (list (scad:cosd (* i 120)) 
 									  (scad:sind (* i 120))))
 				  '(0 1 2 0))))
-   (scad:translate (* side-length (/ (sqrt 3) -6)) 0 0 
-				   (scad:cube (/ side-length 3) 
-							  (/ side-length 3) 
-							  2 :center t))))
+   (scad:translate (* *side-length* (/ (sqrt 3) -6)) 0 0 
+				   (scad:cube (/ *side-length* 3) 
+							  (/ *side-length* 1) 
+							  (* 2 *thickness*) :center t))))
 
+(defun triangle ()
+; since I'm making my triangle by "radiating from the center" one
+; unit, I need to scale by something to get the right side length
+  (let ((s (* *side-length* (/ (sqrt 3) 3))))
+	(scad:scale s s *thickness*
+				(scad:polygon 
+				 (loop for i from 0 to 2 
+					collecting (list (scad:cosd (* i 120)) 
+									 (scad:sind (* i 120))))
+				 '(0 1 2 0)))))
 
 (scad:emit
- (equilateral-triangle 10)
+ (hinged-triangle)
  :file "/home/dr/software/blueprints/3D/openscad/360hinge.scad"
  :fn 20)
 
