d12a8aac9a405d5011042b643b0127c9a442b9e9
David Rysdam <david@rysdam.org>
2014-10-13 22:02:43 -0400

    two hinge halves

--- 3D/openscad/360hinge.lisp
+++ 3D/openscad/360hinge.lisp
@@ -1,19 +1,61 @@
 (load "~/software/blueprints/3D/openscad/opensexp.lisp")
 
-(defparameter *side-length* 10)
-(defparameter *thickness* 2)
+(defparameter *side-length* 30)
+(defparameter *thickness* 5)
+(defparameter *flex-thickness* 2)
+
+(defun hinge-female ()
+  (scad:scad-union
+   (scad:translate (- (/ *side-length* 8) (/ *flex-thickness* 4)) 0 0
+				   (scad:cube (- (/ *side-length* 4) (/ *flex-thickness* 2))
+							  (/ *side-length* 2)
+							  *flex-thickness* 
+							  :center t))
+   (scad:translate (/ *side-length* 4)
+				   0 
+				   0 
+				   (scad:difference
+					(scad:rotate 90 0 0 
+								 (scad:cylinder 
+								  *flex-thickness*
+								  (/ *side-length* 2)
+								  :center t))
+					(scad:rotate 90 0 0 
+								 (scad:cylinder 
+								  (- (/ *flex-thickness* 2) .1)
+								  (/ *side-length* 1.5)
+								  :center t))))))
+
+(defun hinge-male ()
+  (scad:translate (/ *side-length* 8) 0 0 
+				  (scad:translate 0 
+								  (- (/ *side-length* 3) (/ *flex-thickness* 2))
+								  0 
+								  (scad:cube (/ *side-length* 4) 
+											 *flex-thickness*
+											 *flex-thickness* 
+											 :center t))
+				  (scad:translate 0 
+								  (+ (/ *side-length* -3) (/ *flex-thickness* 2))
+								  0 
+								  (scad:cube (/ *side-length* 4) 
+											 *flex-thickness*
+											 *flex-thickness* 
+											 :center t))
+				  (scad:translate (/ *side-length* 8)
+								  0 
+								  0 
+								  (scad:rotate 90 0 0 
+											   (scad:cylinder 
+												(/ *flex-thickness* 2)
+												(/ *side-length* 1.5)
+												:center t)))))
+
 
 (defun hinged-triangle ()
   (scad:scad-union 
-   (triangle)
-   (scad:translate (* *side-length* (/ (sqrt 3) -6)) 0 0 
-   				   (scad:cube (/ *side-length* 4) 
-   							  (/ *side-length* 2)
-   							  (/ *thickness* 4) :center t))
-   (scad:translate (* *side-length* (/ (sqrt 3) -4)) 0 0 
-				   (scad:cube (/ *side-length* 10) 
-							  (/ *side-length* 2)
-							  (/ *thickness* 2) :center t))))
+   (triangle)))
+								  
 
 (defun notched-triangle ()
   ; since I'm making my triangle by "radiating from the center" one
@@ -43,7 +85,10 @@
 				 '(0 1 2 0)))))
 
 (scad:emit
- (hinged-triangle)
+ (scad:scad-union
+  (hinge-female)
+  (hinge-male)
+  )
  :file "/home/dr/software/blueprints/3D/openscad/360hinge.scad"
  :fn 20)
 
