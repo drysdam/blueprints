0bed1e2fb3e00e4a2b3e3278bbdd2fa66606602e
David Rysdam <drysdam@ll.mit.edu>
2014-10-23 07:11:51 -0400

    completely re-thinking design, placement changes

--- 3D/openscad/360hinge.lisp
+++ 3D/openscad/360hinge.lisp
@@ -2,93 +2,48 @@
 
 (defparameter *side-length* 30)
 (defparameter *thickness* 5)
-(defparameter *flex-thickness* 2)
+(defparameter *hinge-width* (/ *side-length* 3))
+(defparameter *hinge-embed* *thickness*)
+(defparameter *center-to-corner* (* *side-length* (/ (sqrt 3) 3)))
+(defparameter *center-to-edge* (/ *center-to-corner* 2))
+(defparameter *center-to-corner-ext* (+ (/ *thickness* 2) *center-to-corner*))
+(defparameter *center-to-edge-ext* (+ (/ *thickness* 2) *center-to-edge*))
 
-(defun hinge-female ()
-  (scad:scad-union
-   (scad:translate (- (/ *side-length* 8) (/ *flex-thickness* 4)) 0 0
-				   (scad:cube (- (/ *side-length* 4) (/ *flex-thickness* 2))
-							  (/ *side-length* 2)
-							  *flex-thickness* 
-							  :center t))
-   (scad:translate (/ *side-length* 4)
-				   0 
-				   0 
-				   (scad:difference
-					(scad:rotate 90 0 0 
-								 (scad:cylinder 
-								  *flex-thickness*
-								  (/ *side-length* 2)
-								  :center t))
-					(scad:rotate 90 0 0 
-								 (scad:cylinder 
-								  (- (/ *flex-thickness* 2) .1)
-								  (/ *side-length* 1.5)
-								  :center t))))))
+(defun hinge ()
+  (scad:rotate 90 0 0
+			   (scad:hull
+				(scad:translate (* -1 *hinge-embed*) 0 (/ *hinge-width* -2)
+								(scad:cylinder (/ *thickness* 2) *hinge-width*))
+				(scad:translate *hinge-embed* 0 (/ *hinge-width* -2)
+								(scad:cylinder (/ *thickness* 2) *hinge-width*)))))
 
-(defun hinge-male ()
-  (scad:translate (/ *side-length* 8) 0 0 
-				  (scad:translate 0 
-								  (- (/ *side-length* 3) (/ *flex-thickness* 2))
-								  0 
-								  (scad:cube (/ *side-length* 4) 
-											 *flex-thickness*
-											 *flex-thickness* 
-											 :center t))
-				  (scad:translate 0 
-								  (+ (/ *side-length* -3) (/ *flex-thickness* 2))
-								  0 
-								  (scad:cube (/ *side-length* 4) 
-											 *flex-thickness*
-											 *flex-thickness* 
-											 :center t))
-				  (scad:translate (/ *side-length* 8)
-								  0 
-								  0 
-								  (scad:rotate 90 0 0 
-											   (scad:cylinder 
-												(/ *flex-thickness* 2)
-												(/ *side-length* 1.5)
-												:center t)))))
-
-
-(defun hinged-triangle ()
-  (scad:scad-union 
-   (triangle)))
-								  
-
-(defun notched-triangle ()
-  ; since I'm making my triangle by "radiating from the center" one
-  ; unit, I need to scale by something to get the right side length
-  (scad:difference
-   (let ((s (* *side-length* (/ (sqrt 3) 3))))
-	 (scad:scale s s *thickness*
-				 (scad:polygon 
-				  (loop for i from 0 to 2 
-					 collecting (list (scad:cosd (* i 120)) 
-									  (scad:sind (* i 120))))
-				  '(0 1 2 0))))
-   (scad:translate (* *side-length* (/ (sqrt 3) -6)) 0 0 
-				   (scad:cube (/ *side-length* 3) 
-							  (/ *side-length* 1) 
-							  (* 2 *thickness*) :center t))))
-
+; this would be a lot simpler as a minkowski hull...
 (defun triangle ()
 ; since I'm making my triangle by "radiating from the center" one
 ; unit, I need to scale by something to get the right side length
-  (let ((s (* *side-length* (/ (sqrt 3) 3))))
-	(scad:scale s s *thickness*
-				(scad:polygon 
-				 (loop for i from 0 to 2 
-					collecting (list (scad:cosd (* i 120)) 
-									 (scad:sind (* i 120))))
-				 '(0 1 2 0)))))
+  (let* ((corners (loop for i from 0 to 2 
+					 collecting (list (* *center-to-corner* (scad:cosd (* i 120)))
+									  (* *center-to-corner* (scad:sind (* i 120)))
+									  0))))
+	(scad:scad-union
+	 (scad:scale 1 1 *thickness*
+				 (scad:polygon (mapcar
+								(lambda (corner)
+								  (list (car corner) (cadr corner)))
+								corners) '(0 1 2 0)))
+	 (scad:pairwise-line corners
+						 (scad:sphere (/ *thickness* 2))))))
+
+(defun hinged-triangle ()
+  (scad:scad-union
+   (scad:translate (- *center-to-edge-ext*) 0 5 (hinge))
+;   (scad:translate (* -2 *hinge-embed*) 0 5 (hinge))
+   (scad:translate (- (* 2 *center-to-edge-ext*)) 0 0 (scad:rotate 0 0 180 (triangle)))
+   (triangle)
+))
 
 (scad:emit
- (scad:scad-union
-  (hinge-female)
-  (hinge-male)
-  )
+  (hinged-triangle)
  :file "/home/dr/software/blueprints/3D/openscad/360hinge.scad"
  :fn 20)
 
