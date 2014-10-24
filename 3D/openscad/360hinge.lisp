0af5837cbeb5188f35c9e374c3d538383e901578
David Rysdam <david@rysdam.org>
2014-10-24 05:22:50 -0400

    dent pins almost perfect

--- 3D/openscad/360hinge.lisp
+++ 3D/openscad/360hinge.lisp
@@ -18,13 +18,13 @@
    (scad:translate 
 	(- *hinge-embed* (/ *thickness* 2)) 
 	0 
-	(/ *hinge-width* 2) 
-	(scad:cylinder *hinge-pin-radius* *hinge-pin-length* :r2 0))
+	(- (/ *hinge-width* 2) (* .1 *hinge-pin-length*))
+	(scad:cylinder *hinge-pin-radius* (* 1.1 *hinge-pin-length*) :r2 0))
    (scad:translate 
 	(- *hinge-embed* (/ *thickness* 2)) 
 	0 
 	(- (/ *hinge-width* -2) *hinge-pin-length*)
-	(scad:cylinder 0 *hinge-pin-length* :r2 *hinge-pin-radius*))))
+	(scad:cylinder 0 (* 1.1 *hinge-pin-length*) :r2 *hinge-pin-radius*))))
 
 (defun hinge ()
   (scad:rotate 
@@ -66,47 +66,46 @@
   ;; scaling-for-clearance has to happen before the translation. again
   ;; arguing for a datastructure that has both the string AND the real
   ;; coordinates
-  (flet ((rotcopy (x) (scad:scad-union (loop for i in sides collecting
-											(scad:rotate 0 0 (* i 120)
-														 x)))))
-	(let* ((hinges (rotcopy (scad:translate (- *center-to-edge-ext*) 0 0 (hinge))))
-		   (hinge-slots (rotcopy (scad:translate (- *center-to-edge-ext*) 0 0 
-												 (scad:scale 1.1 1.05 2 (hinge)))))
+  (flet ((rotcopy (x) (scad:scad-union 
+					   (loop for i in sides collecting
+							(scad:rotate 0 0 (* i 120)
+										 x)))))
+	(let* ((hinges (rotcopy (scad:translate 
+							 (- *center-to-edge-ext*) 0 0 
+							 (hinge))))
+		   (hinge-slots (rotcopy (scad:translate 
+								  (- *center-to-edge-ext*) 0 0 
+								  (scad:scale 1.1 1.1 2 (hinge)))))
 		   (hinge-pins (rotcopy (scad:translate 
-								 (- (- *center-to-edge* (/ *hinge-embed* 2))) 
-								 (/ *hinge-pin-length* 2) 
-								 0 
+								 (- *center-to-edge-ext*) 0 0 
 								 (hinge-pin))))
-		   (hinge-pin-holes (scad:scad-union
-							 (rotcopy (scad:translate 
-									   (- (- *center-to-edge* (/ *hinge-embed* 2))) 
-									   (/ *hinge-pin-length* 2) 
-									   0 
-									   (scad:scale 1.1 1 1.1 (hinge-pin))))
-							 (rotcopy (scad:translate 
-									   (- (+ *center-to-edge* (* 3/2 *hinge-embed*)))
-									   (/ *hinge-pin-length* 2) 
-									   0 
-									   (scad:scale 1.1 1 1.1 (hinge-pin)))))))
-	(scad:scad-union 
-	 hinge-pins
-	 (scad:difference
-	  hinges
-	  hinge-pin-holes)
-	 (scad:difference
-	  (triangle)
-	  hinge-slots
-	  hinge-pin-holes))
-	)))
+		   (hinge-pin-holes 
+			(scad:scad-union
+			 (rotcopy (scad:translate 
+					   (- *center-to-edge-ext*) 0 0
+					   (scad:scale 1.1 1.1 1.1 (hinge-pin))))
+			 (rotcopy (scad:translate 
+					   (- (+ *center-to-edge-ext* *hinge-embed*)) 0 0 
+					   (scad:scale 1.1 1.1 1.1 (hinge-pin)))))))
+	  (scad:scad-union 
+	   hinge-pins
+	   (scad:difference
+	   	hinges
+	   	hinge-pin-holes)
+	   (scad:difference
+		(triangle)
+		hinge-slots
+		hinge-pin-holes))
+	  )))
 
 (scad:emit
+;  (scad:scad-union
+;   (hinge-pin)
+;   (hinge))
  (scad:scad-union
-  (hinge-pin)
-  (hinge))
- ;; (scad:scad-union
- ;;  (hinged-triangle '(0))
- ;;  (scad:translate *triangle-center-distance* 0 0
- ;;  				  (scad:rotate 0 0 180 (hinged-triangle '(0)))))
+  (hinged-triangle '(0))
+  (scad:translate *triangle-center-distance* 0 0
+  				  (scad:rotate 0 0 180 (hinged-triangle '(0)))))
  :file "/home/dr/software/blueprints/3D/openscad/360hinge.scad"
  :fn 20)
 
