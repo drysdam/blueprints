2407dadeb22d05277dedb4618412ecdd55e644cc
David Rysdam <david@rysdam.org>
2014-12-26 05:47:57 -0500

    switch to kyle's letters

--- 3D/openscad/decoder.lisp
+++ 3D/openscad/decoder.lisp
@@ -1,9 +1,11 @@
 (load "opensexp.lisp")
 
+
+
 (defun letter-slice (radius n)
   (scad:rotate 0 0 (* n (/ 360 26))
-			   (scad:translate 0 (- radius 5) 0
-							   (scad:scale .1 .1 .1 "k();"))))
+			   (scad:translate 0 (- radius 10) 3.3
+							   (scad:scale .7 .7 1 "K();"))))
 
 (defun letter-wheel (radius)
   (scad:scad-union 
@@ -14,21 +16,28 @@
 (defun axle ()
   (scad:cylinder 10 10 :center 't))
 
+(scad:emit (letter-wheel 50)
+		   :file "decoder1.scad" 
+		   :includes '("letters.scad"))
+
 (scad:emit 
  (scad:scad-union
-  (scad:translate 0 0 -5 (axle))
+  (scad:difference (scad:translate 0 0 5
+								   (letter-wheel 40))
+				   (scad:scale 1.05 1.05 2 (axle)))
+  (scad:translate 0 0 5 (axle))
   (letter-wheel 55))
- :fn 20
+ :fn 26
  :file "decoder1.scad"
- :includes '("alphanumeric.scad"))
+ :includes '("letters.scad"))
 
 (scad:emit 
  (scad:difference (scad:translate 0 0 -5
 								  (letter-wheel 40))
 				  (scad:scale 1.05 1.05 2 (axle)))
- :fn 20
+ :fn 26
  :file "decoder2.scad"
- :includes '("alphanumeric.scad"))
+ :includes '("letters.scad"))
 
 ;; Local Variables:
 ;; eval: (slime-eval-on-save)
