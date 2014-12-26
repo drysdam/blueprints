346eda108198202d213548956ecf1d32178b8584
David Rysdam <david@rysdam.org>
2014-12-26 09:02:46 -0500

    - found kyle's NEW letters - generated entire alphabet - tweaked rotation to be nicer

--- 3D/openscad/decoder.lisp
+++ 3D/openscad/decoder.lisp
@@ -1,35 +1,29 @@
 (load "opensexp.lisp")
 
-
+(defparameter *alphabet* "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 (defun letter-slice (radius n)
-  (scad:rotate 0 0 (* n (/ 360 26))
-			   (scad:translate 0 (- radius 10) 3.3
-							   (scad:scale .7 .7 1 "K();"))))
+  (let ((letter-f (concatenate 'string (subseq *alphabet* n (1+ n)) "();")))
+	(scad:rotate 0 0 (* -1 n (/ 360 26))
+				 (scad:translate 0 (- radius 10) 3.3
+								 (scad:scale .7 .7 1 letter-f)))))
 
 (defun letter-wheel (radius)
   (scad:scad-union 
    (scad:cylinder radius 3)
-   (loop for i from 1 upto 26 collecting
+   (loop for i from 0 upto 25 collecting
 		(letter-slice radius i))))
 
 (defun axle ()
   (scad:cylinder 10 10 :center 't))
 
-(scad:emit (letter-wheel 50)
-		   :file "decoder1.scad" 
-		   :includes '("letters.scad"))
-
 (scad:emit 
  (scad:scad-union
-  (scad:difference (scad:translate 0 0 5
-								   (letter-wheel 40))
-				   (scad:scale 1.05 1.05 2 (axle)))
   (scad:translate 0 0 5 (axle))
   (letter-wheel 55))
  :fn 26
  :file "decoder1.scad"
- :includes '("letters.scad"))
+ :includes '("NEWletters.scad"))
 
 (scad:emit 
  (scad:difference (scad:translate 0 0 -5
@@ -37,7 +31,7 @@
 				  (scad:scale 1.05 1.05 2 (axle)))
  :fn 26
  :file "decoder2.scad"
- :includes '("letters.scad"))
+ :includes '("NEWletters.scad"))
 
 ;; Local Variables:
 ;; eval: (slime-eval-on-save)
